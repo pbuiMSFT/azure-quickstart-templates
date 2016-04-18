463c2bb8c9a956aa86ccbc1727eacc2ecf5cb3b3
pbuiMSFT <paulbui@microsoft.com>
2016-04-18 12:34:49 -0500

    CustomScripts

---
+++ 201-vm-dynamicdisks-domainjoin/vm_dynamicdisks_domainjoin/vm_dynamicdisks_domainjoin/Templates/windows2012r2.ps1
@@ -0,0 +1,27 @@
+﻿function IsAdmin
+{
+    $IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()` 
+        ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") 
+    
+    return $IsAdmin
+}
+
+function Disable-IEESC
+{
+    $AdminKey = HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}
+    $UserKey = HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}
+    Set-ItemProperty -Path $AdminKey -Name IsInstalled -Value 0
+    Set-ItemProperty -Path $UserKey -Name IsInstalled -Value 0
+    Stop-Process -Name Explorer
+    Write-Host IE Enhanced Security Configuration (ESC) has been disabled. -ForegroundColor Green
+}
+
+"Disable Firewall windows 2012"
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+"Disable IE ESC"
+Disable-IEESC
+
+"Install chocolatey"
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
