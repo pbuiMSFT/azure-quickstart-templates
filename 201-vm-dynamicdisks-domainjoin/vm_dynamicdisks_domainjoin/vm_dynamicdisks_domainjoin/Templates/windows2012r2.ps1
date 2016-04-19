8651930000a1c24871f52632d3ab26a53a7b7a4a
pbuiMSFT <paulbui@microsoft.com>
2016-04-18 22:18:00 -0500

    v1.0
    - Dynamic Disks
    - Domain Join
    - Symantec Extension
    - Custom Extention
    o Disable Firewall
    o Create Storage Pool

--- 201-vm-dynamicdisks-domainjoin/vm_dynamicdisks_domainjoin/vm_dynamicdisks_domainjoin/Templates/windows2012r2.ps1
+++ 201-vm-dynamicdisks-domainjoin/vm_dynamicdisks_domainjoin/vm_dynamicdisks_domainjoin/Templates/windows2012r2.ps1
@@ -6,22 +6,19 @@
     return $IsAdmin
 }
 
-function Disable-IEESC
-{
-    $AdminKey = HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}
-    $UserKey = HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}
-    Set-ItemProperty -Path $AdminKey -Name IsInstalled -Value 0
-    Set-ItemProperty -Path $UserKey -Name IsInstalled -Value 0
-    Stop-Process -Name Explorer
-    Write-Host IE Enhanced Security Configuration (ESC) has been disabled. -ForegroundColor Green
-}
-
 "Disable Firewall windows 2012"
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-"Disable IE ESC"
-Disable-IEESC
 
 "Install chocolatey"
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
+"Create Storage Pools"
+$storagePoolName = "StoragePool"
+$storageSubName = "Storage Spaces*"
+$vDiskName = "VirtualDisk"
+$allocationSize = "65536"
+
+New-StoragePool –FriendlyName $storagePoolName –StorageSubsystemFriendlyName $storageSubName –PhysicalDisks (Get-PhysicalDisk –CanPool $True)
+$disks = Get-StoragePool –FriendlyName $storagePoolName -IsPrimordial $false | Get-PhysicalDisk
+New-VirtualDisk –FriendlyName $vDiskName -ResiliencySettingName Simple –NumberOfColumns ($disks.Count) –UseMaximumSize –Interleave 256KB –StoragePoolFriendlyName $storagePoolName
+Get-VirtualDisk –FriendlyName $vDiskName | Get-Disk | Initialize-Disk –Passthru | New-Partition –AssignDriveLetter –UseMaximumSize | Format-Volume –AllocationUnitSize $allocationSize 
