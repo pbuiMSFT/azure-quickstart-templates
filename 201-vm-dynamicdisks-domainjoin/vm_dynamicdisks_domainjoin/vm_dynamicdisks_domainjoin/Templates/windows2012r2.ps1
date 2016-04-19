ffa695dda1b7263d7159321481f0aca6ddcfe41e
pbuiMSFT <paulbui@microsoft.com>
2016-04-18 23:45:22 -0500

    Change Timezone

--- 201-vm-dynamicdisks-domainjoin/vm_dynamicdisks_domainjoin/vm_dynamicdisks_domainjoin/Templates/windows2012r2.ps1
+++ 201-vm-dynamicdisks-domainjoin/vm_dynamicdisks_domainjoin/vm_dynamicdisks_domainjoin/Templates/windows2012r2.ps1
@@ -6,11 +6,13 @@
     return $IsAdmin
 }
 
+"Set Timezone"
+
 "Disable Firewall windows 2012"
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
 "Install chocolatey"
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
 
 "Create Storage Pools"
 $storagePoolName = "StoragePool"
@@ -21,6 +23,11 @@ $allocationSize = "65536"
 New-StoragePool –FriendlyName $storagePoolName –StorageSubsystemFriendlyName $storageSubName –PhysicalDisks (Get-PhysicalDisk –CanPool $True)
 $disks = Get-StoragePool –FriendlyName $storagePoolName -IsPrimordial $false | Get-PhysicalDisk
 New-VirtualDisk –FriendlyName $vDiskName -ResiliencySettingName Simple –NumberOfColumns ($disks.Count) –UseMaximumSize –Interleave 256KB –StoragePoolFriendlyName $storagePoolName
-Get-VirtualDisk –FriendlyName $vDiskName | Get-Disk | Initialize-Disk –Passthru | New-Partition –AssignDriveLetter –UseMaximumSize | Format-Volume –AllocationUnitSize $allocationSize 
+Get-VirtualDisk –FriendlyName $vDiskName | Get-Disk | Initialize-Disk –Passthru | New-Partition –AssignDriveLetter –UseMaximumSize | Format-Volume –AllocationUnitSize $allocationSize -FileSystem NTFS 
 
-new-item -path C:\temp\ -name "windows2012r2.txt" -itemtype "file" -value "Windows 2012 R2 Script Executed"
+"Change TimeZone to Central Time"
+$timezone = "Central Standard Time"
+&tzutil.exe /s "$timezone"
+
+"Create Filecheck"
+new-item -path C:\temp\ -name "windows2012r2.txt" -itemtype "file" -value "Windows 2012 R2 Script Executed" -Force
